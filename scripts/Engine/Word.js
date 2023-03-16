ec74325d2118360986c27bcc62b84cb0745143ef
edyMartin <edgarmartinx@icloud.com>
2023-03-16 10:50:42 -0600

    fragmentado de codigo en lugares especificos

---
+++ scripts/Engine/Word.js
@@ -0,0 +1,13 @@
+
+export default () => {
+    const world = new OIMO.World({
+        timestep: 1 / 60,
+        iterations: 8,
+        broadphase: 2,
+        worldscale: 1,
+        random: true,
+        info: false
+    });
+
+    return {world}
+}
